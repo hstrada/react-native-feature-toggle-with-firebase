a70856322a6486b4dc46f0f8af0a78852e8a2785
hstrada <h.strada@hotmail.com>
2021-04-15 13:33:08 -0300

    chore: refactor home

--- Remotine/App.tsx
+++ Remotine/App.tsx
@@ -1,54 +1,20 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 
-import remoteConfig from '@react-native-firebase/remote-config';
-
-type TRemoteTypes = 'RemotineHome' | 'RemotineFeature';
-
-const RemoteFeatures: TRemoteTypes[] = ['RemotineHome', 'RemotineFeature'];
-
-const loadRemoteValue = async (key: TRemoteTypes) => {
-  try {
-    return await remoteConfig().getValue(key).asBoolean();
-  } catch (error) {
-    // serviço de log (error)
-  }
-};
-
-const loadRemoteFeatures = () => {
-  return RemoteFeatures.reduce((acc, current) => {
-    return [...acc, {[current]: loadRemoteValue(current)}];
-  }, []);
-};
-
-const fetchRemoteConfig = async () => {
-  try {
-    await remoteConfig().setConfigSettings({
-      minimumFetchIntervalMillis: 30,
-    });
-    // await remoteConfig().setDefaults({
-    //   RemotineHome: false,
-    //   RemotineFeature: false,
-    // });
-
-    await remoteConfig().fetchAndActivate();
-    await loadRemoteFeatures();
-  } catch (error) {
-    // tratamento de erro
-    console.log(error);
-  }
-};
+import {fetchAndActivateRemoteConfig} from './services/firebase';
+import {getRemoteFeatureValue} from './utils/remoteConfig';
 
 const App = () => {
   React.useEffect(() => {
-    fetchRemoteConfig();
+    fetchAndActivateRemoteConfig();
   }, []);
 
-  console.log(loadRemoteFeatures());
+  const isHomeFeatureEnabled = getRemoteFeatureValue('RemotineHome');
 
   return (
-    <View>
-      <Text>Hello</Text>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      {isHomeFeatureEnabled && <Text>sim</Text>}
+      {isHomeFeatureEnabled ? <Text>sim</Text> : <Text>não</Text>}
     </View>
   );
 };
---
+++ Remotine/services/firebase.ts
@@ -0,0 +1,15 @@
+import remoteConfig from '@react-native-firebase/remote-config';
+
+const MINIMUM_FETCH_INTERVAL_MILLIS = 30;
+
+export const fetchAndActivateRemoteConfig = async () => {
+  try {
+    await remoteConfig().setConfigSettings({
+      minimumFetchIntervalMillis: MINIMUM_FETCH_INTERVAL_MILLIS,
+    });
+
+    await remoteConfig().fetchAndActivate();
+  } catch (error) {
+    // tratamento de erro
+  }
+};
